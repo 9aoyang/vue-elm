86cc46ae165ec8812da4ae7a5620612cc4068a13
9aoyang <9aoyang@gmail.com>
2019-01-27 21:42:37 +0800

    :construction: 页面骨架搭建完毕

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <a v-link="{path: '/goods'}">商品</a>
       </div>
@@ -25,12 +25,21 @@ export default {
 };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+@import 'common/css/mixin.styl'
 .tab
   display flex
   width 100%
   height 40px
   line-height 40px
+  // border-bottom 1px solid rgba(7, 17, 27, 0.1)
+  border-1px(rgba(7, 17, 27, 0.1))
   .tab-item
     flex 1
     text-align center
+    & > a
+      display block
+      font-style 14px
+      color rgb(77, 85, 93)
+      &.active
+        color rgb(240, 20, 20)
 </style>
--- src/main.js
+++ src/main.js
@@ -2,18 +2,32 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import App from './App';
-import goods from './components/goods/goods';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import 'common/css/index.styl';
 
 Vue.use(VueRouter);
 
 let app = Vue.extend(App);
 
-let router = new VueRouter();
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
 
 router.map({
   '/goods': {
     component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
   }
 });
 
 router.start(app, '#app');
+
+router.go('/goods');
