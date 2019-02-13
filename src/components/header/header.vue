7d7424257e487808246db30ba6b1cd6f30e15526
9aoyang <9aoyang@gmail.com>
2019-02-13 21:57:05 +0800

    header style

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -16,11 +16,18 @@
         </div>
       </div>
       <div v-if="seller.supports" class="support-count">
-        <span class="count">{{seller.supports.length}}</span>
+        <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%" alt="">
+    </div>
   </div>
 </template>
 <script>
@@ -44,8 +51,9 @@ export default {
 @import '../../common/stylus/mixin'
 
 .header
+  position relative
   color #fff
-  background #000000
+  background rgba(7, 17, 27, 0.5)
   .content-wrapper
     position relative
     padding 24px 12px 18px 24px
@@ -105,7 +113,7 @@ export default {
     .support-count
       position absolute
       right 12px
-      bottom 18px
+      bottom 14px
       padding 0 8px
       height 24px
       line-height 24px
@@ -113,7 +121,45 @@ export default {
       background rgba(0, 0, 0, 0.2)
       text-align center
       .count
+        vertical-align top
         font-size 10px
       .icon-keyboard_arrow_right
+        margin-left 2px
+        line-height 24px
         font-size 10px
+  .bulletin-wrapper
+    position relative
+    height 28px
+    line-height 28px
+    padding 0 22px 0 12px
+    white-space nowrap
+    overflow hidden
+    text-overflow ellipsis
+    background rgba(7, 17, 27, 0.2)
+    .bulletin-title
+      display inline-block
+      vertical-align top
+      margin-top 8px
+      width 22px
+      height 12px
+      bg-image('bulletin')
+      background-size 22px 12px
+      background-repeat no-repeat
+    .bulletin-text
+      vertical-align top
+      margin 0 4px
+      font-size 10px
+    .icon-keyboard_arrow_right
+      position absolute
+      font-size 10px
+      right 12px
+      top 8px
+  .background
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index -1
+    filter blur(10px)
 </style>
