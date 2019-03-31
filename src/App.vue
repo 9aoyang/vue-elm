8a93872d60f9def6ca472a3c4baa2d61ad623998
9aoyang <9aoyang@gmail.com>
2019-04-01 07:56:11 +0800

    渲染商品列表数据

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,6 @@ export default {
       response = response.body;
       if (response.errno === ERR_OK) {
         this.seller = response.data;
-        console.log(this.seller);
       }
     });
   }
@@ -47,7 +46,6 @@ export default {
   width 100%
   height 40px
   line-height 40px
-  // border-bottom 1px solid rgba(7, 17, 27, 0.1)
   border-1px(rgba(7, 17, 27, 0.1))
   .tab-item
     flex 1
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="goods">
-    i am goods
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="(index, good) in goods" :key="index">
+          <span class="text">
+            <span v-show="good.type > 0" class="icon" :class="classMap[good.type]"></span>{{ good.name }}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
 <script>
+const ERR_OK = 0;
+
 export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
   data() {
     return {
-
+      goods: []
     };
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    this.$http.get('/api/goods').then((response) => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.goods = response.data;
+      }
+    });
   },
   components: {
 
@@ -18,4 +43,16 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  .goods
+    display flex
+    position absolute
+    top: 174px
+    bottom 46px
+    width 100%
+    .menu-wrapper
+      flex: 0 0 80px
+      width 80px
+      background #f3f5f7
+    .foods-wrapper
+      flex 1
 </style>
