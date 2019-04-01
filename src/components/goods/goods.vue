4659204c8968f2eefb05981a909ba398e9a0f6de
9aoyang <9aoyang@gmail.com>
2019-04-01 08:14:03 +0800

    商品页左侧布局实现

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,8 +2,8 @@
   <div class="goods">
     <div class="menu-wrapper">
       <ul>
-        <li v-for="(index, good) in goods" :key="index">
-          <span class="text">
+        <li v-for="(index, good) in goods" :key="index" class="menu-item">
+          <span class="text border-1px">
             <span v-show="good.type > 0" class="icon" :class="classMap[good.type]"></span>{{ good.name }}
           </span>
         </li>
@@ -43,6 +43,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import "~common/stylus/mixin";
   .goods
     display flex
     position absolute
@@ -53,6 +54,36 @@ export default {
       flex: 0 0 80px
       width 80px
       background #f3f5f7
+      .menu-item
+        display table
+        height 54px
+        width 56px
+        padding 0 12px
+        line-height 14px
+        .icon
+          display inline-block
+          vertical-align top
+          width 12px
+          height 12px
+          margin-right 2px
+          background-size 12px 12px
+          background-repeat no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display table-cell
+          width 56px
+          vertical-align middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size 12px
     .foods-wrapper
       flex 1
 </style>
